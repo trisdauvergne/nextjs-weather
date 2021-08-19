36c9ac885cfeb9f57b2cc2d896e846595a5c4f07
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-08-19 10:05:12 +0200

    Rendering current stockholm data weather

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,21 +13,3 @@ const Navbar = () => {
 }
  
 export default Navbar;
-
-// var axios = require("axios").default;
-
-// var options = {
-//   method: 'GET',
-//   url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-//   params: {q: 'stockholm'},
-//   headers: {
-//     'x-rapidapi-key': 'b297513b21msh55c6d341915a703p1c4837jsna1b2d91c7b69',
-//     'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
--- pages/results/current.js
+++ pages/results/current.js
@@ -1,13 +1,37 @@
 import WeatherLayout from '../../components/WeatherLayout'
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
+  console.log('data in currentWeather', data);
   return (
   <div>
     <WeatherLayout>
     <h1>Current weather in Stocholm</h1>
+    <p>Last updated at {data.current.last_updated}</p>
+    <p>Weather in {data.location.name} is {data.current.condition.text}</p>
+    <p>Currently {data.current.temp_c} deg c / {data.current.temp_f} deg f</p>
+    <p>Wind speed is {data.current.wind_kph} kph / {data.current.wind_mph} mph</p>
+    <p>Wind direction is {data.current.wind_dir}</p>
+    <p>Humidity is {data.current.humidity}%</p>
     </WeatherLayout>
   </div>
   );
 }
 
-export default CurrentWeather
+export const getServerSideProps = async () => {
+  const res = await fetch("https://weatherapi-com.p.rapidapi.com/current.json?q=stockholm", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "bfc3214c88msh3ff7f5be1d4b52cp15da52jsn664b510a9b59",
+      "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+    }
+  });
+  const data = await res.json();
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default CurrentWeather;
