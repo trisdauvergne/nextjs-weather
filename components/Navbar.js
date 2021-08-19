e0fd57fd5217e1ba1516294ae95d1dee7d700c25
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-08-19 12:08:29 +0200

    3 day weather page rendering data from API

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
   return ( 
     <div>
       <h1>NextJS Weather</h1>
-      <Link href="/"><a>Search</a></Link>
-      <Link href="/results/"><a>Results page</a></Link>
-      <Link href="https://www.weatherapi.com/"><a target="_blank">To the API</a></Link>
+      <Link href="/"><a>Homepage</a></Link>
+      <Link href="/results/"><a>Show the weather</a></Link>
+      <Link href="https://www.weatherapi.com/"><a target="_blank">About the API</a></Link>
     </div>
    );
 }
--- pages/results/current.js
+++ pages/results/current.js
@@ -1,17 +1,17 @@
 import WeatherLayout from '../../components/WeatherLayout'
 
-const CurrentWeather = ({ data }) => {
-  console.log('data in currentWeather', data);
+const CurrentWeather = ({ currentData }) => {
+  console.log('data in currentWeather', currentData);
   return (
   <div>
     <WeatherLayout>
     <h1>Current weather in Stocholm</h1>
-    <p>Last updated at {data.current.last_updated}</p>
-    <p>Weather in {data.location.name} is {data.current.condition.text}</p>
-    <p>Currently {data.current.temp_c} deg c / {data.current.temp_f} deg f</p>
-    <p>Wind speed is {data.current.wind_kph} kph / {data.current.wind_mph} mph</p>
-    <p>Wind direction is {data.current.wind_dir}</p>
-    <p>Humidity is {data.current.humidity}%</p>
+    <p>Last updated at {currentData.current.last_updated}</p>
+    <p>Weather in {currentData.location.name} is {currentData.current.condition.text}</p>
+    <p>Currently {currentData.current.temp_c} deg c</p>
+    <p>Wind speed is {currentData.current.wind_kph} kph</p>
+    <p>Wind direction is {currentData.current.wind_dir}</p>
+    <p>Humidity is {currentData.current.humidity}%</p>
     </WeatherLayout>
   </div>
   );
@@ -25,11 +25,11 @@ export const getServerSideProps = async () => {
       "x-rapidapi-host": `${process.env.NEXT_PUBLIC_API_HOST}`
     }
   });
-  const data = await res.json();
+  const currentData = await res.json();
 
   return {
     props: {
-      data
+      currentData
     }
   }
 }
