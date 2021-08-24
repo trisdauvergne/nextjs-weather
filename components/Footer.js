ac28baa6a375ae8c27b7b5f8fde89173238d4507
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-08-24 18:10:55 +0200

    Completed basic styling on desktop now need to make responsive

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
 
 const Footer = () => {
   return ( 
     <footer>
-      <Link href="https://www.weatherapi.com/"><a target="_blank">About the API</a></Link>
+      <Link href="https://www.weatherapi.com/"><a className={styles.footer} target="_blank">To the API</a></Link>
     </footer>
    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,10 +11,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section>
-        <h1>Weather app</h1>
-        <p>Click <Link href="/current"><a>here</a></Link> to see the current weather in Stockholm (just Stockholm for now)</p>
-        <p>Or click <Link href="/forecasted"><a>here</a></Link> to see the weather (just in Stockholm) over the next 3 days</p>
+      <section className={styles.home}>
+        <p>Weather Vain* focuses on the weather info affecting your appearance. Can I wear shorts today? Will the wind ruin my hair? Is it going to be tanning weather?</p>
+        <br/>
+        <p>Click <Link href="/current"><a className={styles.home__anchor}>here</a></Link> to see the current weather in Stockholm (just Stockholm for now)</p>
+        <br/>
+        <p>Or click <Link href="/forecasted"><a className={styles.home__anchor}>here</a></Link> to see the weather (just in Stockholm) over the next 3 days</p>
       </section>
 
     </div>
