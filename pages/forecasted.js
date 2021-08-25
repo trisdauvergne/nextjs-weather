26bcb90639921cf67d2e27f81cf9f88e6765de9b
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-08-25 16:14:23 +0200

    Basic responsive styling completed on Navbar

--- pages/forecasted.js
+++ pages/forecasted.js
@@ -5,11 +5,11 @@ const Forecasted = ({ forecastedData }) => {
   
   return (
     <section className={styles.forecasted}>
-      <h1 className={styles.forecasted__heading}>3 day forecast</h1>
+      {/* <h1 className={styles.forecasted__heading}>3 day forecast</h1> */}
       <div className={styles.forecasted__forecasts}>
       {forecastedData.forecast.forecastday.map((day, i) =>
         <div className={styles.forecasted__forecast} key={i}>
-          <p className={styles.forecasted__date}>Forecasted weather on {day.date}</p>
+          <p className={styles.forecasted__date}>{day.date}</p>
           <p className={styles.forecasted__subheading}>{day.day.condition.text}</p>
           <p>Chance of rain: {day.day.daily_chance_of_rain}%</p>
           <p>Forecasted temperature: {day.day.avgtemp_c}°c</p>
